06aa0a04b34b6b044b99077ac7ad699c741d886f
Nishacthomas <tthomas2027@amsbronx.org>
2025-07-29 15:47:56 +0000

    added javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Hello world!");
+
+let myString = "a2b3c";
+console.log(myString);
+
+let myNumVar = 123;
+console.log(typeof myNumVar
+
+    console.log(10 + "eggs")
+    console.log(10+7+"eggs");
+    console.log("eggs" +10 + 7);
+
+    console.log(2==2);
+    console.log(7>3)&&(5<10); 
+    
+    //One condition must be true
+    console.log(10>3) || (10>12); 
+
+    //If the statement is logical or not
+    console.log(!5>3); 
+
+
+    function introduction((name, age){
+        console.log("Hello, my name is " + name + "and I am" + age + " years old.");
+    }
+    introduction("Tanisha" ,15)
+
+
+    const hour = new Date().getHours();
+    let greeting;"";
+
+    if(hour<12){
+        greeting = "Good morning🌞"; 
+    }else if (hour<18){
+        greeting = "Good afternoon🌞";
+    }else { 
+        greeting= " Good evening🌙";
+    
+    }
+
+document.getElementById("greeting").innerText= greeting;
+    
+    
